312e25a6ed87407b7a857f70fd63f12dce7a5908
PhantomOffKanagawa <harry@surmafamily.com>
2025-03-16 21:09:31 -0500

    Initial Commit

---
+++ MidtermProjectTodoistClone/MidtermProjectTodoistCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermProjectTodoistCloneApp.swift
+//  MidtermProjectTodoistClone
+//
+//  Created by Harry Surma on 3/16/25.
+//
+
+import SwiftUI
+
+@main
+struct MidtermProjectTodoistCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MidtermProjectTodoistCloneTests/MidtermProjectTodoistCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermProjectTodoistCloneTests.swift
+//  MidtermProjectTodoistCloneTests
+//
+//  Created by Harry Surma on 3/16/25.
+//
+
+import Testing
+@testable import MidtermProjectTodoistClone
+
+struct MidtermProjectTodoistCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
